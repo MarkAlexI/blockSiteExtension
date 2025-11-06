83833e9b420ecb0451e118a33953cd7b6114f03c
MarkAlexI <markovaleksandr108@gmail.com>
2025-11-06 16:00:11 +0200

    fix(analytics): extend statistics collection to include site redirects

---
+++ scripts/redirect.js
@@ -0,0 +1,26 @@
+try {
+  const url = new URL(window.location.href);
+  const fromUrl = url.searchParams.get('from');
+  const toUrl = url.searchParams.get('to');
+  
+  if (fromUrl && toUrl) {
+    try {
+      chrome.runtime.sendMessage({
+        type: 'record_redirect',
+        from: fromUrl,
+        to: toUrl
+      });
+    } catch (error) {
+      console.error('Error sending redirect record message:', error);
+    }
+    
+    location.replace(toUrl);
+    
+  } else {
+    console.warn('Redirect page called without "from" or "to" params.');
+    location.replace(chrome.runtime.getURL("blocked.html"));
+  }
+} catch (error) {
+  console.error('Error in redirect script:', error);
+  location.replace(chrome.runtime.getURL("blocked.html"));
+}
