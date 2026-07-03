a97a95a2b4aae0f0cedb6f992b88f56508732d97
MarkAlexI <markovaleksandr108@gmail.com>
2026-07-03 04:06:46 +0300

    feat(focus): close matching tabs when starting a focus session

--- utils/createInstallURL.js
+++ utils/createInstallURL.js
@@ -1,5 +1,5 @@
 export function createInstallURL() {
-  const base = "https://blockdistraction.com/index.html";
+  const base = "https://blockdistraction.com/";
   
   const browser = "chrome";
   const version = chrome.runtime.getManifest().version;
