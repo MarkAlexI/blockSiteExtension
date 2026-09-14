a324aed01e90616d753f6230926617d4c6e2ee7b
MarkAlexI <markovaleksandr108@gmail.com>
2026-09-14 21:49:14 +0300

    fix: remove automatic context from uninstall URL

--- utils/updateUninstallURL.js
+++ utils/updateUninstallURL.js
@@ -1,16 +1,3 @@
 export async function updateUninstallURL() {
-  const { credentials } = await chrome.storage.sync.get(['credentials']);
-  const { rules } = await chrome.storage.local.get(['rules']);
-
-  const rawData = JSON.stringify({
-    l: credentials?.isLegacyUser ?? null,
-    d: credentials?.installationDate ?? null,
-    p: credentials?.isPro ?? null,
-    v: chrome.runtime.getManifest().version ?? null,
-    r: rules?.length ?? null
-  });
-
-  const encodedData = btoa(unescape(encodeURIComponent(rawData)));
-  
-  chrome.runtime.setUninstallURL(`https://blockdistraction.com/uninstall.html?ctx=${encodedData}`);
+  await chrome.runtime.setUninstallURL('https://blockdistraction.com/uninstall.html');
 }
